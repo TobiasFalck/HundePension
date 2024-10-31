0c92b5a9f7c6d4464d0786d16dbc7cf0d40b7a0d
TobiasFalck <TobiasFalck13@gmail.com>
2024-10-31 09:37:35 +0100

    Making Problems

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,67 @@
 package org.example;
 
-import org.example.City.CityDao;
-import org.example.Dog.Dog;
 import org.example.Dog.DogDao;
 import org.example.Dog.DogDaoImpl;
-import org.example.EventLog.EventLog;
-import org.example.EventLog.EventLogDao;
-import org.example.EventLog.EventlogDaoImpl;
 import org.example.Owner.Owner;
 import org.example.Owner.OwnerDao;
 import org.example.Owner.OwnerDaoImpl;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.util.Scanner;
 
 public class Main
 {
 
+    private static final Scanner scanner = new Scanner(System.in);
+    private static boolean exit = false;
+    private static Owner currentOwner = null;
+
+
     public static void main(String[] args) throws Exception
     {
         System.out.println("Welcome to the Dog Daycare");
-        Scanner scanner = new Scanner(System.in);
-        String interfaceId = null;
+
+        String firstMenu = null;
+        OwnerDao oDao = new OwnerDaoImpl();
+        DogDao dDao = new DogDaoImpl();
+
+
+        checkUserStatus(oDao);
+
+
+
+        while(!exit)
+        {
+            private static void checkUserStatus(OwnerDao oDao)
+            {
+                boolean isUserValid = false;
+
+                while(!isUserValid){
+                    System.out.println("Are you an existing Owner? (yes/no)");
+                    String userResponse = scanner.nextLine();
+
+                    switch(userResponse)
+                    {
+                        case "yes":
+                            isUserValid = true;
+                            break;
+
+                        case "no":
+                            isUserValid = false;
+                            break;
+
+                        default:
+                            System.out.println("please try again");
+                    }
+
+                }
+            }
+
+            private static boolean validateOwner(OwnerDao oDao)
+            System.out.println("Pleaser Enter your ID: ");
+            int ownerId = Integer.parseInt(scanner.nextLine());
+
+        }
+
 
 
 
